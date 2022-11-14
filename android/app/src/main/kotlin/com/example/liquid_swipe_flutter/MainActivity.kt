763eeba48a788d1d3307d341016de46c7ce16e86
Gagandeep <gagandeepkaur1523@gmail.com>
2022-11-14 17:56:47 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/liquid_swipe_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.liquid_swipe_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/liquid_swipe.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LiquidSwipe extends StatefulWidget {
+  const LiquidSwipe({super.key});
+
+  @override
+  State<LiquidSwipe> createState() => _LiquidSwipeState();
+}
+
+class _LiquidSwipeState extends State<LiquidSwipe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
